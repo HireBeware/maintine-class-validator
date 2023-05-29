7933a7bbf63b730ad281d3dece38f262aca0b84a
David Oelfke <doelfke@gmail.com>
2023-05-28 22:31:58 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # maintine-class-validator
+
 Use class-validator with Mantine forms
+
+#### Example
+
+```
+const form = useForm<CreateReviewDTO>({
+    dto: new CreateReviewDTO(),
+    initialValues: {
+        title: props.myReview?.title || '',
+        rating: props.myReview?.rating || 0,
+        review: props.myReview?.review || '',
+        reviewNameType: ReviewNameType.Anonymous
+      },
+    transform: (dto: CreateReviewDTO) => {
+        dto.rating = parseInt(dto.rating as unknown as string) || 0;
+        dto.reviewNameType = parseInt(dto.reviewNameType as unknown as string) || 0;
+        return dto;
+      }
+});
+```
+
+#### Extra
+
+There is type safety with form fields, by accessing form.fields:
+
+```
+<TextInput label="Title" {...form.getInputProps(form.fields.title)}/>
+```
---
+++ useForm.tsx
@@ -0,0 +1,61 @@
+import { useForm as mantineUseForm } from '@mantine/form';
+import { validateSync } from 'class-validator';
+import { Enum } from 'typescript-string-enums';
+
+type Entries<T> = {
+    [K in keyof T]: T[K];
+ };
+
+export const useForm = <T extends object>(args: { 
+      dto: T, 
+      initialValues: T,
+      validateInputOnBlur?: boolean,
+      clearInputErrorOnChange?: boolean
+      validateInputOnChange?: boolean,
+      transform?: (values: Entries<T>) => Entries<T> }) => {
+
+   const mantineForm = mantineUseForm<Entries<T>>({
+      initialValues : args.initialValues,
+      validateInputOnBlur: args.validateInputOnBlur,
+      clearInputErrorOnChange: args.clearInputErrorOnChange,
+      validateInputOnChange: args.validateInputOnChange,
+
+      
+      validate: (values) => {
+
+         const transformedValues = args.transform ? args.transform(values) : values;
+         Object.keys(transformedValues).forEach((key) => {
+         // @ts-ignore
+            args.dto[key] = transformedValues[key];
+         });
+         const validationErrors = validateSync(args.dto);
+
+         const formErrors: Record<string, string> = {};
+
+         validationErrors.forEach((error)=> {
+            const errorMessageArray = Object.values(error.constraints!)[0].split(' ');
+            errorMessageArray.shift();
+            let errorMessage = errorMessageArray.join(' ');
+            errorMessage = errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
+            formErrors[error.property] = errorMessage;
+         });
+
+         return formErrors;
+      },
+      transformValues(values) {
+         return args.transform ? args.transform (values) : values;
+      }
+   });
+
+   const keys = Object.keys(args.dto) ;
+   
+   const fields = Enum<string>(...keys) as {
+      [K in keyof T]: string
+   };
+
+   return {
+      ...mantineForm,
+      fields
+   };
+   
+};
